1f1e43783efc1c071600b825451662a4bc749138
c_recker2023 <c_recker@coloradocollege.edu>
2024-11-05 15:19:00 -0700

    11/5 3:18, current attempt at concat

--- back_end_data/concatinate_v1.py
+++ back_end_data/concatinate_v1.py
@@ -7,6 +7,7 @@ import seaborn as sns
 #https://github.com/americanpanorama/mapping-inequality-census-crosswalk
 
 ##pullting the mapping_inequality data
+'''
 den_gpd = gpd.read_file("mapping_inequality/geojson_DEN.json")
 print(den_gpd["grade"])
 ##pulling the FEMA data
@@ -17,3 +18,25 @@ risk_den_data = den_data[["RISK_VALUE", "RISK_SCORE", "RISK_RATNG"]]
 print(risk_den_data)
 grade_to_risk = pd.concat([risk_den_data, den_gpd["grade"]])
 print(grade_to_risk)
+'''
+#mapping inequality
+mapping = gpd.read_file("/Users/CassidyRecker/Desktop/github/FinalProject/back_end_data/mapping_inequality/MIv3Areas_2020TractCrosswalk.geojson")
+#print(mapping.columns)
+denver_data = mapping[mapping["city"] == "Denver"]
+denver_grades = denver_data[["grade", "GEOID"]].dropna().drop_duplicates()
+#denver_grades["GEOID"] = denver_grades['GEOID'].astype(str)
+#denver_grades["GEOID"] = denver_grades["GEOID"][1:]
+#denver_grades["GEOID"] = denver_grades['GEOID'].astype(int)
+denver_grades.index = denver_grades["GEOID"]
+print(denver_grades)
+#FEMA
+colo_data = pd.read_csv("FEMA_data/NRI_Table_CensusTracts_Colorado.csv")
+den_data = colo_data[colo_data["COUNTY"]=="Denver"]
+denver_risks = den_data[["RISK_VALUE", "TRACTFIPS"]].dropna().drop_duplicates()
+denver_risks.index = den_data["TRACTFIPS"]
+print(denver_risks)
+###GEOID should MAP to TRACTFIPS
+#overall = pd.concat([denver_risks, denver_grades], axis = 1)
+#overall = pd.concat([denver_risks,denver_grades])
+overall = denver_grades.combine_first(denver_risks)
+print(overall)
