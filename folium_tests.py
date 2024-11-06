03f3c8f34abf20e792b9dcc0a645f83c2d6c08ac
J-628 <96267350+J-628@users.noreply.github.com>
2024-11-06 14:12:59 -0700

    Add files via upload
    Testing adding files directly because I do not know how GitHub works (this is my folium code plus the GeoJSON dataset plus the resulting map)

---
+++ folium_tests.py
@@ -0,0 +1,26 @@
+import geopandas
+import folium
+
+gjf = geopandas.read_file("mappinginequality.json")
+#print(gjf)
+
+testmap = folium.Map(tiles="cartodb positron")
+#testmap = folium.Map(tiles="stamenterrain", attr="test")
+#tileset = 'https://{s}.tile.thunderforest.com/neighbourhood/{z}/{x}/{y}.png?apikey={apikey}'
+#copyline = '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+#testmap = folium.Map(tiles=tileset, attr=copyline)
+
+redlining_data = "mappinginequality.json"
+
+#print(gjf["grade"])
+for i in range( len( gjf["geometry"] ) ):
+    zone = gjf["geometry"][i]
+    grade_color = gjf["fill"][i]
+    folium.GeoJson(zone, color=grade_color, weight=1, fill_opacity=0.5).add_to(testmap)
+
+#for i in range( len( gjf["geometry"] ) ):
+#    if gjf["city"][i] == "Colorado Springs":
+#        print(gjf.loc[i])
+
+testmap.fit_bounds( [(25,-125), (50,-70)] )
+testmap.save("test_map.html")
