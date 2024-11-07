7842e1765079f18d30efc1bb4125e0556acefa50
c_recker2023 <c_recker@coloradocollege.edu>
2024-11-07 14:30:21 -0700

    11/7 2:30 pulls all the data from the fema folder

--- back_end_data/getting_all_fema.py
+++ back_end_data/getting_all_fema.py
@@ -15,5 +15,15 @@ fema_dir = "FEMA_data/"
 for fema in os.listdir(fema_dir):
     files.append(fema_dir + fema)
 
-print(files)
-print(len(files))
+fema_data = pd.read_csv(files[0])
+fema_data = fema_data[["RISK_VALUE", "TRACTFIPS"]].dropna().drop_duplicates()
+for index in range (1, len(files)):
+    current = pd.read_csv(files[index])
+    current = current[["RISK_VALUE", "TRACTFIPS"]].dropna().drop_duplicates()
+    print("current is")
+    print(current.shape)
+    fema_data = pd.concat([fema_data, current])
+    print("fema is")
+    print(fema_data.shape)
+print("overall is")
+print(fema_data.shape)
