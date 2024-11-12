aae8bc5169d9715fc576a4bcb189e6598ade9c25
tessa <t_olsen2024@coloradocollege.edu>
2024-11-12 10:28:36 -0700

    11/12 10:28 comments added for presentation

--- back_end_data/overall_dataframeV2.py
+++ back_end_data/overall_dataframeV2.py
@@ -9,18 +9,25 @@ import time
 
 def fema_df():
     '''
-    going to use every file in FEMA data to create giant dataframe
+    going to loop through every FEMA data csv file to create giant dataframe
     https://www.geeksforgeeks.org/python-loop-through-folders-and-files-in-directory/
     '''
     files = []
-    #"CFLD_RISKS", "HWAV_RISKS", "HRCN_RISKS", "RFLD_RISKS", "TRND_RISKS", "WFIR_RISKS"
-    values = ["RISK_SCORE", "TRACTFIPS"]
+
+    # making a dataframe that just holds the tractfips, composite risk score,
+    # and selected natural hazard risk scores for the fema data
+    values = ["RISK_SCORE", "TRACTFIPS", "CFLD_RISKS", "HWAV_RISKS", "HRCN_RISKS",
+        "RFLD_RISKS", "TRND_RISKS", "WFIR_RISKS"]
     fema_dir = "FEMA_data/"
+
+    # making a list of all of the FEMA files
     for fema in os.listdir(fema_dir):
         files.append(fema_dir + fema)
 
     fema_data = pd.read_csv(files[0])
     fema_data = fema_data[values].dropna().drop_duplicates()
+
+    # using pandas to pull wanted data from every file
     for index in range (1, len(files)):
         print("adding: ")
         print(files[index])
@@ -32,31 +39,38 @@ def fema_df():
 def mapping_df():
     values = ["grade", "GEOID", "city", "state", "geometry"]
     mapping_data = gpd.read_file("mapping_inequality/MIv3Areas_2020TractCrosswalk.geojson")
-    #making a data that just holds the GEOID and grades for the redlined data
+
+    # making a dataframe that just holds the GEOID, grades, city, state, and geometry
+    # for the redlined data
     mapping_data = mapping_data[values].dropna().drop_duplicates()
     mapping_data["GEOID"] = mapping_data["GEOID"].astype(int)
+
+    '''
+    #https://stackoverflow.com/questions/27965295/dropping-rows-from-dataframe-based-on-a-not-in-condition
+    print(mapping_data["grade"].value_counts(normalize=True))
+    going to drop them because they make less than 4% of data'''
 
     grades_list = ['A','B','C','D']
     mapping_data = mapping_data[mapping_data['grade'].isin(grades_list)]
     return mapping_data
 
 
-#def preprocess(fema_values = ["RISK_VALUE"], mapping_values = ["grades"] ):
-    ###FEMA###
+
 def preprocess(fema_data, mapping_data):
-
+    # changing column titles for TRACTFIPS and GEOID in order to concatinate
     risks = fema_data.rename(columns={"TRACTFIPS": "area"})
-    print("risk shape")
-    print(risks)
-    grades = mapping_data.rename(columns={"GEOID": "area"}).dropna()
-    print("grades shape")
-    print(grades)
 
+    grades = mapping_data.rename(columns={"GEOID": "area"}).dropna()
+
+    # concatinating both datasets on area column
     overall = pd.merge(grades, risks, on="area")
     return overall
 
+
+
 t0 = time.time()
 output = preprocess(fema_df(), mapping_df())
+print(output)
 t1= time.time()
 pd.set_option("display.max_rows", None)
 
