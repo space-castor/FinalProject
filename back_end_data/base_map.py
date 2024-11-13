c5c52b90b237d6fbe761e7b063441f9ed6274efb
space-castor <saicastor.college@gmail.com>
2024-11-13 08:01:46 -0700

    ack

--- back_end_data/base_map.py
+++ back_end_data/base_map.py
@@ -3,40 +3,58 @@ import folium
 import branca
 import requests
 import io
-from risk_by_state import *
+from overall import *
+from jinja2 import Template
 
 # MAIN DATA FILE
 # pulls method to create the dataframe from risk_by_state to be accessible to this file
 overall, num_states  = preprocess()
 
-# COLOR FUNCTIONS / DICTIONARIES
+base = folium.Map([43, -100], zoom_start=4)
+
+# COLOR FUNCTIONS
 
 # sets up a color dictionary associated with the different scores
 # referenced from foliums user guide on geojson popups and tooltips
+
+# state level color functions
 climate_map = branca.colormap.LinearColormap(
     vmin=overall["RISK_SCORE"].quantile(0.0),
     vmax=overall["RISK_SCORE"].quantile(1),
-    colors=["orange", "yellow", "lightblue", "blue"],
+    colors=["yellow", "orange", "red"],
     caption= "Areas Climate Risk Score",
 )
 
-'''
-def color_risk(value):  # scalar value defined in 'column'
-    if value == "HWAV_RISKS":
-        return "#00bf7d"
-    elif value == "HRCN_RISKS":
-        return "#00b4c5"
-    elif value == "CFLD_RISKS" :
-        return "#8dd2dd"
-    elif value == "RFLD_RISKS":
-        return "#0073e6"
-    elif value == "TRND_RISKS":
-        return "#2546f0"
-    elif value == "WFIR_RISKS" :
-        return "#5928ed"
+def color_grade (value):
+    colorlist = ["#b3c7f7", "#8babf1", "#0073e6", "#054fb9"]
+    if value == "A":
+        return colorlist[0]
+    elif value == "B":
+        return colorlist[1]
+    elif value == "C" :
+        return colorlist[2]
+    elif value == "D":
+        return colorlist[3]
     else:
         return "white"
-'''
+
+# country view color function
+def color_risk(value):  # scalar value defined in 'column'
+    colorlist = ["#00bf7d", "#00b4c5", "#8dd2dd", "#0073e6", "#2546f0", "#5928ed"]
+    if value == "HWAV_RISKS":
+        return colorlist[0]
+    elif value == "HRCN_RISKS":
+        return colorlist[1]
+    elif value == "CFLD_RISKS" :
+        return colorlist[2]
+    elif value == "RFLD_RISKS":
+        return colorlist[3]
+    elif value == "TRND_RISKS":
+        return colorlist[4]
+    elif value == "WFIR_RISKS" :
+        return colorlist[5]
+    else:
+        return "white"
 
 # GEOMETRY / TRANSLATION RESOURCES
 
@@ -70,33 +88,114 @@ state_list = overall["state"].unique()
 for st in state_list:
     #overall is a dict returing data for each state for display
     state_dict = findmax(st, overall)
+    state_dict["name"] = st
     index = list(states_plot["name"].keys())[list(states_plot["name"].values()).index(abbr[st])]
     state_dict["geometry"] = states_plot["geometry"][index]
     cv_df.append(state_dict)
+    img = makeplot(st, "RISK_SCORE", overall)
+    state_dict["image"] = img
 
 # reconvert into data frame
 cv_df = gpd.GeoDataFrame(cv_df, crs="EPSG:4326")
 
-# Base Map Set Up / Country Level Information
+# Country Map Set Up
+for i in range(len(cv_df["name"])):
+    zone = cv_df["geometry"][i]
+    risk_color = color_risk(cv_df["max"][i])
+
+    # tooltip set up
+    fields=["name", "n", "max" , "HWAV_RISKS", "HRCN_RISKS", "RFLD_RISKS",
+        "TRND_RISKS", "WFIR_RISKS", "CFLD_RISKS"]
+    aliases=["State: ", "Number of areas recorded: ", "Highest Risk: ", "Heatwave Risk: ", "Hurricane Risk:",
+        "Rainflood Risk: ", "Tornado Risk: ", "Wildfire Risk: ", "Coastal Flooding Risk: "]
+    tooltip_text = "\n" + str([aliases[x] + str(cv_df[fields[x]][i]) + "\n" for x in range(len(fields))])
+
+    img = cv_df["image"][i]
+    html_string = "hello"
+   #f"<img src = {{img}} alt= "State violin plot">"
+
+    popup = folium.Popup(html_string, lazy = True)
+
+    tooltip = folium.Tooltip(
+        tooltip_text,
+        style="""
+            background-color: #F0EFEF;
+            border: 2px solid black;
+            border-radius: 3px;
+            box-shadow: 3px;
+        """)
+
+    tooltip = folium.Tooltip(tooltip_text)
+
+    folium.GeoJson(zone, color=risk_color, weight=1, fill_opacity=0.5, popup = popup, tooltip = tooltip).add_to(base)
+''''
+country_view = folium.GeoJson(
+    cv_df,
+    style_function = lambda feature: {
+        "fillColor": color_risk(feature["properties"]["max"]),
+        "fillOpacity": 0.5,
+    },
+    tooltip = tooltip
+).add_to(base)
+'''
+'''
+# as prettu as this is, this is not compatible with branca elements so
 base = cv_df.explore(
     column = "max",  # make choropleth based on "max" column
     scheme = "Quantiles",
     cmap = "Blues",
     legend=True,  # show legend
     popup= False,  # show popup (on-click)
-    # marker_type = folium.Marker(),
+    marker_type = folium.Marker(),
 #    legend_kwds=dict(colorbar=False),  # do not use colorbar
     name="country_view",  # name of the layer in the map
 )
 climate_map.add_to(base)
+'''
 
 # --------- STATE LEVEL -----------
 # personalized ->  generated at the user input
-# temporary function to only pull up state data -> to be replaced with preprocess method with state parameter
+
+#  function to only pull up state data
 def pull(state, state_list):
     try:
         if state in state_list:
             raise IndexError
+
+        tooltip = folium.GeoJsonTooltip(
+            fields=["state", "city", "RISK_SCORE", "grade", "category"],
+            aliases=["State:", "City:", "Risk Score:", "Grade:", "Category:"],
+            localize=True,
+            sticky=False,
+            labels=True,
+            style="""
+                background-color: #F0EFEF;
+                border: 2px solid black;
+                border-radius: 3px;
+                box-shadow: 3px;
+            """,
+            max_width=800,
+        )
+
+        popup = folium.GeoJsonTooltip(
+            fields=["state", "city", "RISK_SCORE", "grade", "category", "HWAV_RISKS", "HRCN_RISKS", "RFLD_RISKS",
+                "TRND_RISKS", "WFIR_RISKS", "CFLD_RISKS"],
+            aliases=["State:", "City:", "Risk Score:", "Grade:", "Category:", "Heatwave Risk:", "Hurricane Risk:",
+                "Rainflood Risk:", "Tornado Risk:", "Wildfire Risk:", "Coastal Flooding Risk:"],
+            localize=True,
+            sticky= True,
+            labels=True,
+            style="""
+                background-color: #F0EFEF;
+                border: 2px solid black;
+                border-radius: 3px;
+                box-shadow: 3px;
+            """,
+            max_width=800,
+            lazy = True,
+        )
+
+        # climate change layer
         new_state = folium.GeoJson(
             overall[(overall["state"] == state)],
             style_function=lambda x: {
@@ -104,13 +203,35 @@ def pull(state, state_list):
                 if x["properties"]["RISK_SCORE"] is not None
                 else "transparent",
                 "color": "black",
-                "fillOpacity": 0.7,
+                "fillOpacity": 0.4,
             },
             control = True,
             smooth_factor = 1.0,
-            name = state,
-            zoom_on_click = True
+            name = state + "-climate-risk",
+            zoom_on_click = True,
+            tooltip = tooltip,
+            popup = popup
         ).add_to(base)
+
+        # red-lining layer
+        new_state = folium.GeoJson(
+            overall[(overall["state"] == state)],
+            style_function=lambda x: {
+                "fillColor": color_grade(x["properties"]["grade"])
+                if x["properties"]["grade"] is not None
+                else "transparent",
+                "color": "black",
+                "fillOpacity": 0.4,
+            },
+            control = True,
+            smooth_factor = 1.0,
+            name = state + "-redlining",
+            zoom_on_click = True,
+            tooltip = tooltip,
+            popup = popup
+        ).add_to(base)
+
+
     except IndexError:
         print(state + " has already been added to your map.")
     except:
@@ -118,21 +239,11 @@ def pull(state, state_list):
 
 
 '''
-# ---- STATE LEVEL --------
 city_risks = overall.groupby(["city"], as_index = False)["RISK_SCORE"].mean()
 disolved_city = overall.dissolve(by = ["state", "city"], method = "unary")
 
-g = folium.GeoJson(
-    statesmerge,
-    style_function=lambda x: {
-        "fillColor": colormap(x["properties"]["RISK_SCORE"])
-        if x["properties"]["RISK_SCORE"] is not None
-        else "transparent",
-        "color": "black",
-        "fillOpacity": 0.4,
-    }
-).add_to(base)
 '''
+
 """ processing everything
 g = folium.GeoJson(
     overall,
@@ -177,6 +288,7 @@ def userInteract():
             state = input("Okay! What state would you like to add? (abbreviations please!) ")
             pull(state, states)
             states.append(state)
+            print("Done!")
 
         if action == "see":
             print("The following states added are : " +  str(states))
@@ -185,6 +297,7 @@ def userInteract():
             file_name = input("Okay! What would you like to name your file? ")
             folium.LayerControl().add_to(base)  # use folium to add layer control
             base.save((file_name + ".html"))
+            print("Done!")
 
         # not an infinite loop!
         if action == "quit":
